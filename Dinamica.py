901c60c09c1313bcdd050ebf29de0760db45c239
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2023-10-24 17:29:19 -0300

    Add files via upload

---
+++ Dinamica.py
@@ -0,0 +1,79 @@
+def op(x, y):
+    if x == 'a' and y == 'a':
+        return 'b'
+    elif x == 'a' and y == 'b':
+        return 'b'
+    elif x == 'a' and y == 'c':
+        return 'a'
+    
+    if x == 'b' and y == 'a':
+        return 'c'
+    elif x == 'b' and y == 'b':
+        return 'b'
+    elif x == 'b' and y == 'c':
+        return 'a'
+        
+    if x == 'c' and y == 'a':
+        return 'a'
+    elif x == 'c' and y == 'b':
+        return 'c'
+    elif x == 'c' and y == 'c':
+        return 'c'
+
+    else:
+        return None
+
+
+#   a b c
+# a b b a
+# b c b a
+# c a c c
+
+X = {'a', 'b', 'c'}
+Y = {'b','c'}
+Z = {'a','b'}
+W = {'a','c'}
+A = {'a'}
+B = {'b'}
+C = {'c'}
+
+def SETMULT(J, K):
+    H = set()
+    for x in J:
+        for y in K:
+            z = op(x, y)
+            H.add(z)
+    return H
+
+# print (SETMULT(C,Y))
+
+def R(s):
+    if len(s) == 1:
+        return {s[0]}
+    else:
+        results = set()
+        for i in range(1, len(s)):
+            left_set = R(s[:i])
+            right_set = R(s[i:])
+            results |= SETMULT(left_set, right_set)
+        return results
+# Al usar 2 R(s) y un setmult la cota superior es approx O(n3)
+
+def Rr(s, memo={}):
+    if len(s) == 1:
+        return {s[0]}
+    elif s in memo:
+        return memo[s]
+    else:
+        results = set()
+        for i in range(1, len(s)):
+            left_set = Rr(s[:i], memo)
+            right_set = Rr(s[i:], memo)
+            results |= SETMULT(left_set, right_set)
+        memo[s] = results
+        return results
+s = 'aaaa'
+
+
+print (R(s))
+print (Rr(s))
